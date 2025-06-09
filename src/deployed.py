6312b1944268c65da4a50df0cff88eb73bdb684a
Emanuel Reinaldo <80110729+emanuelrgb01@users.noreply.github.com>
2025-06-09 08:45:59 -0300

    Now user can test with py deployed.py <path.csv>

--- src/deployed.py
+++ src/deployed.py
@@ -2,6 +2,7 @@ import joblib
 import pandas as pd
 from sklearn.metrics import accuracy_score, classification_report
 import sys
+import os
 
 TARGET_COLUMN_NAME = "Churn"
 TEST_DATASET_PATH = "apartments_for_rent_final_test.csv"
@@ -11,17 +12,27 @@ MOCK_PATH = "data/lab2_2025_dataset.csv"
 MOCK_MODEL_PATH = "src/model.joblib"
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Uso correto: python deployed.py <caminho_para_csv>")
+        sys.exit(1)
+
+    test_dataset_path = sys.argv[1]
+
+    if not os.path.isfile(test_dataset_path):
+        print(f"Erro: Arquivo '{test_dataset_path}' não encontrado.")
+        sys.exit(1)
+
     try:
         loaded_model = joblib.load(MODEL_PATH)
-        df = pd.read_csv(TEST_DATASET_PATH).dropna()
+        df = pd.read_csv(test_dataset_path).dropna()
 
     except FileNotFoundError as e:
         print(
-            "Arquivo não encontrado. Verifique se o dataset ou o arquivo do modelo estão na mesma pasta deste arquivo"
+            "Erro: Modelo não encontrado. Certifique-se de que 'model.joblib' está presente."
         )
         sys.exit(1)
     except Exception as e:
-        print(f"Erro: {e}")
+        print(f"Erro ao carregar o dataset ou modelo: {e}")
         sys.exit(1)
 
     X = df.drop(TARGET_COLUMN_NAME, axis=1)
